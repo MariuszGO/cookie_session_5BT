8b29fc8b9a79bc85e3d6a3de9981073df8afd217
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-09-24 12:08:09 +0200

    Cookie
    Pliki Cookie

---
+++ cookie.php
@@ -0,0 +1,32 @@
+<?php
+
+$im = $_GET['imie'];
+$naz = $_GET['nazwisko'];
+//echo $im;
+
+$cookie_name = "dane";
+//$cookie_value = $im . " " .  $naz;
+
+
+
+?>
+<html>
+<body>
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+    $cookie_value= 0;
+    setcookie($cookie_name, $cookie_value, time() + 5, "/");
+    //$_COOKIE[$cookie_name] = " ";
+  echo "Witaj: ". $im . ' ' .$naz . " jesteś po raz pierwszy na stornie";
+} else {
+    $cookie_value = $_COOKIE[$cookie_name];
+    $cookie_value++;
+    setcookie($cookie_name, $cookie_value, time() + 5, "/");
+    
+    echo "Witaj: ".$_COOKIE[$cookie_name] . " byłeś już na tej stornie";
+}
+?>
+
+</body>
+</html>
