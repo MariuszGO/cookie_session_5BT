ae299866dec2dd4ddf3d3bf642503368492f15d6
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-09-24 13:03:29 +0200

    N2

--- logowanie.php
+++ logowanie.php
@@ -1,9 +1,12 @@
 <?php
+$login=$_POST["login"];
+$pass=$_POST["pass"];
+
 if (isset($login) && isset($pass)){
-  if ($login == "lockoom" && $pass == "test"){
+  if ($login == "admin" && $pass == "admin"){
     session_start();
-    session_register("login");
-    header("Location: strona.php?" . SID);
+    $_SESSION["login"] = $login; 
+    header("Location: strona.php?");
     exit();
   } else
     $error = "<B>Błędny login lub hasło!</B><BR>";
@@ -15,13 +18,10 @@ if (isset($login) && isset($pass)){
   <TITLE>Logowanie</TITLE>
 </HEAD>
 <BODY>
-<?php
-  echo $error ? $error : "";
-?>
   <B>Podaj login i&nbsp;hasło</B>
-  <FORM method="POST" action="<?php echo $PHP_SELF?>">
+  <FORM method="POST" action="logowanie.php">
     Login: <INPUT type="text" name="login"><BR>
-    Hasło: <INPUT type="password" name="pass"><BR>
+    Hasło: <INPUT type="text" name="pass"><BR>
     <INPUT type="submit" value="Zaloguj się">
   </FORM>
 </BODY>
--- strona.php
+++ strona.php
@@ -1,28 +1,15 @@
 <?php
-if (isset($login) && isset($pass)){
-  if ($login == "lockoom" && $pass == "test"){
-    session_start();
-    session_register("login");
-    header("Location: strona.php?" . SID);
-    exit();
-  } else
-    $error = "<B>Błędny login lub hasło!</B><BR>";
-} else
-  $error = false;
+session_start();
 ?>
 <HTML>
 <HEAD>
-  <TITLE>Logowanie</TITLE>
+  <TITLE>Wylogowanie</TITLE>
 </HEAD>
 <BODY>
 <?php
-  echo $error ? $error : "";
+  echo "Użytkownik " . $_SESSION["login"] . " jest zalogowany ";
+  //echo " został wylogowany.";
+ // session_destroy();
 ?>
-  <B>Podaj login i&nbsp;hasło</B>
-  <FORM method="POST" action="<?php echo $PHP_SELF?>">
-    Login: <INPUT type="text" name="login"><BR>
-    Hasło: <INPUT type="password" name="pass"><BR>
-    <INPUT type="submit" value="Zaloguj się">
-  </FORM>
 </BODY>
 </HTML>
