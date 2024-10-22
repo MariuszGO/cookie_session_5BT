f11d28d993c993d3240c9609ecb435b37e6b9c53
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-10-22 11:39:58 +0200

    js4

--- js1.js
+++ js1.js
@@ -1,3 +1,13 @@
 function moja_funkcja1(){
     document.getElementById("nazwa").innerHTML = "To jest działanie funkcji z pliku js1.js";
  }
+
+
+ function suma(){
+    let a=10;
+    let b=20;
+    let c;
+    let tekst ="Nasza";
+    c=a+b;
+    document.getElementById("suma").innerHTML = tekst + " suma wynosi: " + c;    
+ }
